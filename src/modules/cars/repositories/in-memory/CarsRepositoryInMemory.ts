910cf8ff45908790b60773e809461e9acbdca607
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-03-01 23:21:59 -0300

    Listagens de carros disponíveis

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -5,6 +5,7 @@ import { ICarsRepository } from "../ICarsRepository";
 
 class CarsRepositoryInMemory implements ICarsRepository {
     cars: Car[] = [];
+
     async create({
         brand,
         category_id,
@@ -34,6 +35,27 @@ class CarsRepositoryInMemory implements ICarsRepository {
     async findByLicensePlate(license_plate: string): Promise<Car> {
         // Para cada carro ira procurar se existe uma placa
         return this.cars.find((car) => car.license_plate === license_plate);
+    }
+
+    async findAvailable(
+        brand?: string,
+        category_id?: string,
+        name?: string
+    ): Promise<Car[]> {
+        // find = retorna 1 objeto -- filter retorna uma lista
+        const all = this.cars.filter((car) => {
+            if (
+                car.available === true &&
+                ((brand && car.brand === brand) ||
+                    (category_id && car.category_id === category_id) ||
+                    (name && car.name === name))
+            ) {
+                return car;
+            }
+            return null;
+        });
+
+        return all;
     }
 }
 
